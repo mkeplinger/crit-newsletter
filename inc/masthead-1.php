df2452e4a5e50444538fd0b2e0e3c90f02b175d0
Kris Sarrat <flinx777@gmail.com>
2013-12-31 13:10:52 -0600

    Masthead updates

---
+++ inc/masthead-1.php
@@ -0,0 +1,24 @@
+			<div class="masthead-layout layout-bar-left color-brown">
+			
+				<div class="masthead-bar-left">&nbsp;</div>
+				
+				<div class="masthead-content-right">
+			
+					<div class="vol-headline">Crittenden’s</div><!-- .vol-headline -->
+					
+					<div class="title-box">
+						<h1>Insurance Markets</h1>
+						<p>Crittenden Research Inc., P.O. BOX 1150, Novato, California 94948, (800) 421 - 3483</p>
+					</div><!-- .title-box -->
+					
+					<div class="title-second-box">
+						&nbsp;
+					</div><!-- .title-second-box -->
+				
+				</div><!-- .masthead-content-right -->
+				
+				<div class="masthead-content-right">
+					&nbsp;
+				</div><!-- .masthead-content-box-right -->
+			
+			</div><!-- .masthead-layout -->
